c16c1e940dab15dfab111c961eab0ae6782ab51d
HyunSung-Na <oceana57@naver.com>
2021-05-22 14:41:32 +0900

    feat: 단일 책임 원칙으로 분리

--- src/main/java/com/iteratrlearning/shu_book/chapter_02/BankStatementAnalyzerSimple.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_02/BankStatementAnalyzerSimple.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementAnalyzerSimple {
@@ -11,17 +13,37 @@ public class BankStatementAnalyzerSimple {
     private static final String RESOURCES = "src/main/resources/";
 
     public static void main(final String[] args) throws IOException {
+            final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
             final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
             final List<String> lines = Files.readAllLines(path);
-            double total = 0d;
 
-            for(final String line: lines) {
-                String[] columns = line.split(",");
-                double amount = Double.parseDouble(columns[1]);
-                total += amount;
+            final List<BankTransaction> bankTransactions
+                    = bankStatementCSVParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+    public static List<BankTransaction> selectInMonth(List<BankTransaction> bankTransactions, Month month) {
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
             }
+        }
 
-            System.out.println("The total for all transactions is " + total);
+        return bankTransactionsInMonth;
+    }
+
+    public static double calculateTotalAmount(List<BankTransaction> bankTransactions) {
+        double total = 0d;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+
+        return total;
     }
 }
 
