b1293aa44176ce1a41f38b52198dad580e2411b5
HyunSung-Na <oceana57@naver.com>
2021-06-17 22:16:16 +0900

    test: step1 실패하는 테스트 만들기

---
+++ src/test/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngineTest.java
@@ -0,0 +1,25 @@
+package com.iteratrlearning.shu_book.chapter_05;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusinessRuleEngineTest {
+
+    @Test
+    void shouldHaveNoRulesInitially() {
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+
+        assertEquals(0, businessRuleEngine.count());
+    }
+
+    @Test
+    void shouldTwoActions() {
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+
+        businessRuleEngine.addAction(() -> {});
+        businessRuleEngine.addAction(() -> {});
+
+        assertEquals(2, businessRuleEngine.count());
+    }
+}
