219ee4a832d80e6cf88529a423269c9919ea81a3
HyunSung-Na <oceana57@naver.com>
2021-06-19 00:03:28 +0900

    test: Inspector Test add

---
+++ src/test/java/com/iteratrlearning/shu_book/chapter_05/InspectorTest.java
@@ -0,0 +1,39 @@
+package com.iteratrlearning.shu_book.chapter_05;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class InspectorTest {
+
+    @Test
+    public void inspectOneConditionEvaluatesTrue() {
+
+        final Facts facts = new Facts();
+        facts.addFact("jobTitle", "CEO");
+        final ConditionAction conditionalAction = new JobTitleCondition();
+        final Inspector inspector = new Inspector(conditionalAction);
+
+        final List<Report> reportList = inspector.inspect(facts);
+
+        assertEquals(1, reportList.size());
+        assertEquals(true, reportList.get(0).isPositive());
+
+
+    }
+
+    private static class JobTitleCondition implements ConditionAction {
+
+        @Override
+        public void perform(Facts facts) {
+
+        }
+
+        @Override
+        public boolean evaluate(Facts facts) {
+            return "CEO".equals(facts.getFacts("jobTitle"));
+        }
+    }
+}
