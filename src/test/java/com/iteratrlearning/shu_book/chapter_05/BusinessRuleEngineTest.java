f9c8d28d55fcdd5c2bf58917e77306d8ce0bc5dc
HyunSung-Na <oceana57@naver.com>
2021-06-18 23:07:40 +0900

    feat: Stage Enum add

--- src/test/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngineTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngineTest.java
@@ -28,7 +28,22 @@ public class BusinessRuleEngineTest {
                 System.out.println(name);
             }
         });
-        businessRuleEngine.addAction(facts -> {});
+        businessRuleEngine.addAction(facts -> {
+            double forecastedAmount = 0.0;
+            Stage dealStage = Stage.valueOf(facts.getFacts("stage"));
+            double amount = Double.parseDouble(facts.getFacts("amount"));
+
+            if (dealStage == Stage.LEAD) {
+                forecastedAmount = amount * 0.2;
+            } else if (dealStage == Stage.EVALUATING) {
+                forecastedAmount = amount * 0.5;
+            } else if (dealStage == Stage.INTERESTED) {
+                forecastedAmount = amount * 0.8;
+            } else if (dealStage == Stage.CLOSED) {
+                forecastedAmount = amount;
+            }
+            facts.addFact("forecastedAmount", String.valueOf(forecastedAmount));
+        });
 
         assertEquals(2, businessRuleEngine.count());
     }
